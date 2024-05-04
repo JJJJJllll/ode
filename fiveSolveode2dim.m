ceb495133396f46bd20db3a30f35701728bdb3c1
JJJJJllll <jishilong200005@gmail.com>
2024-05-04 11:36:24 +0800

    7936 iter best 0.1+0.1v

---
+++ fiveSolveode2dim.m
@@ -0,0 +1,83 @@
+clear
+load('data.mat')
+
+poola = linspace(0,3,31);
+poolb = linspace(0,1.5,16);
+poolc = linspace(0,1.5,16);
+L = {poola,poolb,poolc};
+n = length(L);
+[L{:}] = ndgrid(L{end:-1:1});
+L = cat(n+1,L{:});
+L = fliplr(reshape(L,[],n));
+% tout = [];
+% yout = [];
+result = [];
+global a;
+global b;
+global c;
+global targetHeight;
+% for j = 1:length(L)
+j = 273;
+a = L(j,1);
+b = L(j,2);
+c = L(j,3);
+total_obj = 0;
+for i = 1:5
+t_real = cell2mat(data(i,1));
+x = cell2mat(data(i,2));
+y = cell2mat(data(i,3));
+z = cell2mat(data(i,4));
+vx = cell2mat(data(i,5));
+vy = cell2mat(data(i,6));
+vz = cell2mat(data(i,7));
+px0 = x(1);
+py0 = y(1);
+pz0 = z(1);
+vx0 = vx(1);
+vy0 = vy(1);
+vz0 = vz(1);
+range = find(z<0.5);
+t_target = t_real(range(1));
+x_target = x(range(1));
+y_target = y(range(1));
+targetHeight = z(range(1));
+options = odeset('Events',@events,'OutputFcn',@odeplot,'OutputSel',[3],...
+   'Refine',4);
+% options = odeset('Events',@events,'Refine',4);
+y0 = [px0;py0;pz0;vx0;vy0;vz0];
+% Solve until the first terminal event.
+figure
+[t,y,te,ye,ie] = ode45(@f,[0 10],y0,options);
+hold on
+plot(t_real,z)
+
+% Accumulate output
+t_diff = abs(t_target - te);
+xy_diff = sqrt((x_target - ye(1))^2 + (y_target - ye(2))^2);
+obj = t_diff + xy_diff;
+total_obj = total_obj + obj;
+end
+result = [result; obj];
+% tout = [tout; t'];
+% yout = [yout; y'];
+% end
+% --------------------------------------------------------------------------
+
+function dydt = f(t,y)
+% y: column vector
+global a;
+global b;
+global c;
+v = [y(4);y(5);y(6)];
+acc =  - a * sign(v) - b * v - c * v * norm(v) + [0;0;-9.8];
+dydt = [v;  acc];
+end
+
+% --------------------------------------------------------------------------
+
+function [value,isterminal,direction] = events(t,y)
+global targetHeight;
+value = y(3) - targetHeight;     % when value passes through zero
+direction = -1;   % in a decreasing direction
+isterminal = 1;   % stop integration
+end
